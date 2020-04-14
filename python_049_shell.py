7202f8cc171f3d026dfd6803eaded1a64f14205f
Soyoung-Yoon <38801594+Soyoung-Yoon@users.noreply.github.com>
2020-04-15 08:31:05 +0900

    update 049, upload 050

--- python_049_shell.py
+++ python_049_shell.py
@@ -1,35 +1,61 @@
 Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> a = 10
->>> b = 20
->>> a == b
+>>> not True
 False
->>> a >= b
-False
->>> a % 2 == 0
+>>> not False
 True
->>> not (a % 2)
+>>> not None
 True
->>> a % 2 == 1
+>>> not 0
+True
+>>> not 0.0
+True
+>>> not ''
+True
+>>> not ()
+True
+>>> not []
+True
+>>> not {}
+True
+>>> not 1
 False
->>> a % 2
+>>> not -1
+False
+>>> not 3.14
+False
+>>> not 'abc'
+False
+>>> True and True
+True
+>>> True and False
+False
+>>> False and True
+False
+>>> False and False
+False
+>>> 1 and 0
 0
->>> a % 3 == 0
-False
->>> not (a%3)
-False
->>> 80 < a < 90
-False
->>> a > 80 and a < 90
-False
->>> a is b
-False
->>> type(a) is int and type(b) is str
-False
->>> type(a) is int and type(b) is int
+>>> 1 and 3.14
+3.14
+>>> 0 and 0
+0
+>>> 0 and 3.14
+0
+>>> True or True
 True
->>> ( type(a) is int ) and (type(b) is int)
+>>> True or False
 True
->>> a>100 or a<0
+>>> False or True
+True
+>>> False or False
 False
+>>> 1 or 0
+1
+>>> 2 or 1
+2
+>>> '' or 3.14
+3.14
+>>> 0 or (1, 2, 3)
+(1, 2, 3)
 >>> 
---
+++ python_050_shell.py
@@ -0,0 +1,35 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 10
+>>> b = 20
+>>> a == b
+False
+>>> a >= b
+False
+>>> a % 2 == 0
+True
+>>> not (a % 2)
+True
+>>> a % 2 == 1
+False
+>>> a % 2
+0
+>>> a % 3 == 0
+False
+>>> not (a%3)
+False
+>>> 80 < a < 90
+False
+>>> a > 80 and a < 90
+False
+>>> a is b
+False
+>>> type(a) is int and type(b) is str
+False
+>>> type(a) is int and type(b) is int
+True
+>>> ( type(a) is int ) and (type(b) is int)
+True
+>>> a>100 or a<0
+False
+>>> 
